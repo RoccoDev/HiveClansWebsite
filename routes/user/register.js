bde48115f3a5c9c3ad809eae20e2ac3909320b3f
RoccoDev <roccodev.business@gmail.com>
2018-09-24 18:53:13 +0200

    Add json response

--- routes/user/register.js
+++ routes/user/register.js
@@ -4,16 +4,25 @@ const User = require("../../user/user.js")
 const bcrypt = require('bcrypt');
 
 router.post('/', (req, res) => {
-   var user = new User({
-       name: req.body.name,
-       password: bcrypt.hashSync(req.body.password, 10)
-   })
 
-    user.save((err) => {
-        if(err) throw err
-        console.log("User saved")
-        res.json({success: true})
+    User.find({name: req.body.name}, function(err, users) {
+        if(!err && users.length != 0) {
+            res.status(409).json({success: false})
+            return;
+        }
+        var user = new User({
+            name: req.body.name,
+            password: bcrypt.hashSync(req.body.password, 10)
+        })
+
+        user.save((err) => {
+            if(err) throw err
+            console.log("User saved")
+            res.json({success: true})
+        })
     })
+
+
 })
 
 
